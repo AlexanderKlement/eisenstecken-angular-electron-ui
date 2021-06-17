2ad93f123e7bdd279ba951c8d22645f39ad64c09
alexander.klement@github.com <Kallegit123>
2021-06-17 17:37:18 +0200

    added raw menu tiles

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -16,9 +16,10 @@ import {MatButtonModule} from "@angular/material/button";
 import {ReactiveFormsModule} from "@angular/forms";
 import { NoteComponent } from './note/note.component';
 import { SingleNoteComponent } from './note/single-note/single-note.component';
+import {MenuTilesComponent} from "./menu-tiles/menu-tiles.component";
 
 @NgModule({
-  declarations: [HomeComponent, ChatComponent, ChatMessageComponent, NoteComponent, SingleNoteComponent],
+  declarations: [HomeComponent, ChatComponent, ChatMessageComponent, NoteComponent, SingleNoteComponent, MenuTilesComponent],
   imports: [CommonModule, SharedModule, HomeRoutingModule, MatCardModule, FlexLayoutModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, ReactiveFormsModule]
 })
 export class HomeModule {}
