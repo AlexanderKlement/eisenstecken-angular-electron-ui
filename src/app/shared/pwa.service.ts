26e77d28c400c6aff9c9e0ec8c747437f0e3dcfa
Ivan Waldboth <csaq5507@uibk.ac.at>
2021-10-19 15:07:27 +0200

    Refactor file location for PWA

--- src/app/shared/pwa.service.ts
+++ src/app/shared/pwa.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Platform } from '@angular/cdk/platform';
 import {MatBottomSheet} from '@angular/material/bottom-sheet';
-import {PwaDialogComponent} from '../pwa-dialog/pwa-dialog.component';
+import {PwaDialogComponent} from './components/pwa-dialog/pwa-dialog.component';
 import {ElectronService} from '../core/services';
 import {timer} from 'rxjs';
 import {take} from 'rxjs/operators';
@@ -18,17 +18,11 @@ export class PwaService {
   }
 
   public initPwaPrompt() {
-    console.log('electron? '+this.electron.isElectron);
-    console.log('platform android '+this.platform.ANDROID);
-    console.log('platform ios '+this.platform.ANDROID);
-    console.log('platform  webkit'+this.platform.WEBKIT);
-    console.log('platform isBrowser '+this.platform.isBrowser);
     if(this.electron.isElectron)
       {return;}
     if (this.platform.ANDROID) {
       window.addEventListener('beforeinstallprompt', (event: any) => {
         event.preventDefault();
-        console.log(event);
         this.promptEvent = event;
         this.openPromptComponent('android');
       });
@@ -36,7 +30,6 @@ export class PwaService {
     if (this.platform.IOS) {
       // @ts-ignore
       const isInStandaloneMode = ('standalone' in window.navigator) && (window.navigator.standalone);
-      console.log('is in standaalone:'+ isInStandaloneMode);
       if (!isInStandaloneMode) {
         this.openPromptComponent('ios');
       }
@@ -44,8 +37,6 @@ export class PwaService {
   }
 
   private openPromptComponent(mobileType: 'ios' | 'android') {
-    console.log('Open: '+mobileType);
-    console.log(this.promptEvent);
     timer(3000)
       .pipe(take(1))
       .subscribe(() => this.bottomSheet.open(PwaDialogComponent, { data: { mobileType, promptEvent: this.promptEvent } }));
